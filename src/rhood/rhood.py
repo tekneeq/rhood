4d830466baf6293ceed216a5a10c8072ab1bbd85
Eugene Park <eugenepark3@gmail.com>
2023-05-03 10:16:40 -0400

    rhood

--- src/rhood/rhood.py
+++ src/rhood/rhood.py
@@ -1,1 +1,20 @@
 import robin_stocks as rs
+from secrets import config
+
+"""
+Trading algo:
+    - 1 hourly: whenever we hit 200 EMA whil 50 EMA is higher, buy the bounce
+
+    
+    Need SPY prices ending every N min
+        60 min for 1h
+        1440 min for daily
+"""
+
+
+rs.robinhood.authentication.login(
+    username=config.USERNAME,
+    password=config.PASSWORD,
+    expiresIn=172800,
+    by_sms=True,
+)
