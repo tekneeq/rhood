6aa4b41efcbd390a44a20523a73fbe5ff0393ae3
Eugene Park <eugenepark3@gmail.com>
2023-05-04 10:06:08 -0400

    rhood

--- src/rhood/rhood.py
+++ src/rhood/rhood.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+from pytz import timezone
 import robin_stocks as rs
 from secrets import config
 
@@ -11,10 +14,33 @@ Trading algo:
         1440 min for daily
 """
 
+ITER_DELAY = 5
+TICKER = "SPY"
 
-rs.robinhood.authentication.login(
-    username=config.USERNAME,
-    password=config.PASSWORD,
-    expiresIn=172800,
-    by_sms=True,
-)
+
+def main():
+    rs.robinhood.authentication.login(
+        username=config.USERNAME,
+        password=config.PASSWORD,
+        expiresIn=172800,
+        by_sms=True,
+    )
+    tz = timezone("EST")
+
+    while True:
+        ctime = datetime.datetime.now(tz).strftime("%Y-%m-%dT%H:%M:%S")
+        ticker_price = round(
+            float(
+                rs.robinhood.stocks.get_latest_price(
+                    TICKER, priceType=None, includeExtendedHours=True
+                )[0]
+            ),
+            2,
+        )
+        print(f"{ctime}: {TICKER} {ticker_price}")
+
+        time.sleep(ITER_DELAY)
+
+
+if __name__ == "__main__":
+    main()
